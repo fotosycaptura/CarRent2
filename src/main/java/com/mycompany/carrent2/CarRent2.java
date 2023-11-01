5abb6741fee51f1813bf94849a71ec59d0d9ce38
fotosycaptura <charsxavier@gmail.com>
2023-11-01 16:13:46 -0300

    Se agrega devolución fallida

--- src/main/java/com/mycompany/carrent2/CarRent2.java
+++ src/main/java/com/mycompany/carrent2/CarRent2.java
@@ -56,28 +56,46 @@ public class CarRent2 {
         System.out.println("Fecha devolucion 'Ingresada' (Fecha del arriendo más los días que indicó al generar el arriendo) :" + fechaFormateada);
         
         //Se procede con la validación y si es correcto, la devolución.
-        a1.generarDevolucion(autoDevuelto, fechaDevolucion);
-        //Se visualiza la devolución
-        System.out.println("Detalle de la devolución: "+ a1.getDevolucion());
-        //Se genera el voucher o ticket
-        a1.generarTicketDeArriendo();
+        if (a1.generarDevolucion(autoDevuelto, fechaDevolucion)){
+            System.out.println("La devolución se realizó correctamente");
+            //Se visualiza la devolución
+            System.out.println("Detalle de la devolución: "+ a1.getDevolucion());
+            
+            //Se genera el voucher o ticket
+            a1.generarTicketDeArriendo();
+        }else{
+            System.out.println("No se pudo realizar la devolución. Compruebe que sea efectivamente el mismo vehículo.");
+        }
+        
         System.out.println("");
         
+        //*************************** Devolución FALLIDA con su vehículo que no corresopnde ********************
+        //se genera una devolución fallida
+        Vehiculo autoDevueltoConFallas = new Vehiculo("XX-YY-25", "KIAdo", "HGF", 2017);
+        fechaDevolucion = new GregorianCalendar();
+        fechaDevolucion.add(fechaDevolucion.DAY_OF_MONTH, a1.getDias());
+       
+        //Se procede con la validación y si es correcto, la devolución.
+        if (a1.generarDevolucion(autoDevueltoConFallas, fechaDevolucion)){
+            System.out.println("La devolución se realizó correctamente");
+        }else{
+            System.out.println("No se pudo realizar la devolución. Compruebe que sea efectivamente el mismo vehículo.");
+        }
         
-//        //*************************** Arriendo 2 FALLIDO con su vehículo y/o cliente deshabilitado********************
-//        System.out.println("**********************************************************************************");
-//        Vehiculo v2 = new Vehiculo("XX-YY-24", "Toyota", "CROSS", 2023);
-//        v2.setCondicion('M');
-//        System.out.println("Vehículo generado: " + v2);
-//        //Se genera una nueva instancia de un cliente
-//        //Por defecto el nuevo cliente está vigente desde el constructor
-//        Cliente c2 = new Cliente("22222222-2", "Jhon Titor 2");
-//        c2.DeshabilitarCliente();
-//        System.out.println("Cliente generado (deshabilitado): " + c2);
-//        Arriendo a2 = new Arriendo(11, f, 5, v2, c2);
-//        
-//        //Se ingresa el arriendo. O eso debería, pero se lanza una excepción porque el vehículo generado está en mantención. Línea 52
-//        //Además el cliente está deshabilitado línea 57
-//        a2.IngresarArriendo();
+        //*************************** Arriendo 2 FALLIDO con su vehículo y/o cliente deshabilitado********************
+        System.out.println("**********************************************************************************");
+        Vehiculo v2 = new Vehiculo("XX-YY-24", "Toyota", "CROSS", 2023);
+        v2.setCondicion('M');
+        System.out.println("Vehículo generado (asignado a mantención): " + v2);
+        //Se genera una nueva instancia de un cliente
+        //Por defecto el nuevo cliente está vigente desde el constructor
+        Cliente c2 = new Cliente("22222222-2", "Jhon Titor 2");
+        c2.DeshabilitarCliente();
+        System.out.println("Cliente generado (deshabilitado): " + c2);
+        Arriendo a2 = new Arriendo(11, f, 5, v2, c2);
+        
+        //Se ingresa el arriendo. O eso debería, pero se lanza una excepción porque el vehículo generado está en mantención. Línea 52
+        //Además el cliente está deshabilitado línea 57
+        a2.IngresarArriendo();
     }
 }
